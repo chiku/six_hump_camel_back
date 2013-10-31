7f43a779999d6d4f2b4bbca79db96584a130002f
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-10-31 23:00:02 +0530

    Cleaner output for main

--- main.rb
+++ main.rb
@@ -6,9 +6,9 @@ def camel_hump_back_problem
   population = Population.new(50, 2, [-3.0, -2.0], [3.0, 2.0], lambda {|x, y|  ( 4.0 - 2.1*x*x +  x*x*x*x/3.0) * x*x + x*y + ( -4.0  +  4.0*y*y) * y*y })
   vector, value, generations = population.differential_evolution(200000, 0.000001)
 
-  print "\n\nThe solution vector is: \t", vector[0], "\t", vector[1]
-  print "\nThe answer is:\t", value
-  print "\nGenerations run:\t",generations
+  puts "\n\nThe solution vector is: \t", vector[0], "\t", vector[1]
+  puts "\nThe answer is:\t", value
+  puts "\nGenerations run:\t",generations
 end
 
 camel_hump_back_problem
