cb65f692c4d4592491d5229cdcda974183100921
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-10-31 23:30:30 +0530

    Contraints can return a random value from its range

--- lib/constraint.rb
+++ lib/constraint.rb
@@ -1,10 +1,19 @@
 class Constraint
   def initialize(options)
-    @min = options[:min]
-    @max = options[:max]
+    @min        = options[:min]
+    @max        = options[:max]
+    @randomizer = -> { rand }
   end
 
   def range
     @max - @min
+  end
+
+  def randomizer=(randomizer)
+    @randomizer = randomizer
+  end
+
+  def rand
+    (@randomizer.call * range) + @min
   end
 end
--- test/constraint_test.rb
+++ test/constraint_test.rb
@@ -3,9 +3,18 @@ require File.expand_path("../test_helper", __FILE__)
 require File.expand_path("../../lib/constraint", __FILE__)
 
 describe "Constraint" do
+  let(:constraint) { Constraint.new(max: 2.0, min: -1.0) }
+
   describe "#range" do
     it "is the difference between the maximun and minimum" do
-      Constraint.new(max: 2.4, min: -1.1).range.must_equal 3.5
+      constraint.range.must_equal 3.0
+    end
+  end
+
+  describe "#rand" do
+    it "is a value between inside the range" do
+      constraint.randomizer = -> { 0.5 }
+      constraint.rand.must_equal 0.5
     end
   end
 end
